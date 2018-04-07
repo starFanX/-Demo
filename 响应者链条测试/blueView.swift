8b314fd11a19150177dc92be0ca14415c3174a40
fanxing <fanxing@sunlands.com>
2018-04-07 22:43:20 +0800

    first commit

---
+++ 响应者链条测试/blueView.swift
@@ -0,0 +1,21 @@
+//
+//  blueView.swift
+//  响应者链条测试
+//
+//  Created by 星 Fan on 2018/2/22.
+//  Copyright © 2018年 星 Fan. All rights reserved.
+//
+
+import UIKit
+
+class blueView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
